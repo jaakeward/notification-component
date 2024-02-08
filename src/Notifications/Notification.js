941076f2e3efb499d94b78c2e7f29dc6566606ec
jaakeward <jpward8930@gmail.com>
2024-02-08 11:44:46 -0500

    Update Notification.js
    Notification stays alive by default, timer starts on mouse enter for each notification
    handleStartTimer is now setID, with interval of 50ms
    Notification close handled by width with useEffect

--- src/Notifications/Notification.js
+++ src/Notifications/Notification.js
@@ -1,63 +1,42 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
-const Notification = props => {
-  const [exit, setExit] = useState(false);
-  const [width, setWidth] = useState(0);
-  const [intervalID, setIntervalID] = useState(null);
-
-  const handleStartTimer = () => {
-    const id = setInterval(() => {
-      setWidth(prev => {
-        if (prev < 100) {
-          return prev + 0.5;
+const Notification = (props) => {
+    const [width, setWidth] = useState(0);
+    const [exit, setExit] = useState(false);
+    const [intervalID, setIntervalID] = useState(0);
+    const setID = (() => {
+        const id = setInterval(() => {
+            setWidth((width) => {
+                if (width < 100){
+                    return width + 0.5;
+                }
+                return width;
+            });
+        }, 50);
+        setIntervalID(id);
+        return id;
+    });
+    React.useEffect(() => {
+       if ((width === 100)) {
+            clearInterval(intervalID);
+            setExit(true);
+            setTimeout(() => {
+                props.dispatch({
+                    type: "REMOVE_NOTIFICATION",
+                    id: props.id,
+                })
+            }, 400);
         }
-
-        clearInterval(id);
-        return prev;
-      });
-    }, 20);
-
-    setIntervalID(id);
-  };
-
-  const handlePauseTimer = () => {
-    clearInterval(intervalID);
-  };
-
-  const handleCloseNotification = () => {
-    handlePauseTimer();
-    setExit(true);
-    setTimeout(() => {
-      props.dispatch({
-        type: "REMOVE_NOTIFICATION",
-        id: props.id
-      })
-    }, 400)
-  };
-
-  React.useEffect(() => {
-    if (width === 100) {
-      // Close notification
-      handleCloseNotification()
-    }
-  }, [width])
-
-  React.useEffect(() => {
-    handleStartTimer();
-  }, []);
-
-  return (
-    <div
-      onMouseEnter={handlePauseTimer}
-      onMouseLeave={handleStartTimer}
-      className={`notification-item ${
-        props.type === "SUCCESS" ? "success" : "error"
-      } ${exit ? "exit" : ""}`}
-    >
-      <p>{props.message}</p>
-      <div className={"bar"} style={{ width: `${width}%` }} />
-    </div>
-  );
+    }, [width]);
+    return (
+        <div 
+            className={`notification-item ${props.type}${exit ? " exit" : ""}`}
+            onMouseLeave={() => {setID();}}
+            onMouseEnter={() => {clearInterval(intervalID);}}>
+            <p>${props.message}</p>
+            <div className="bar" style={{width: `${width}%`}} />
+        </div>
+    );
 };
 
 export default Notification;
