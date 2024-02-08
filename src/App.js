0f293b1dd3a813d708954a014da4950ace22fdcb
jaakeward <jpward8930@gmail.com>
2024-02-08 11:37:26 -0500

    Update App.js
    Added functionality for adding new notifications using button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import React, {useState} from 'react';
+import { useContext } from 'react';
 import './App.css';
-import {useNotification} from "./Notifications/NotificationProvider";
+import { useCreateNotification } from './Notifications/NotificationProvider';
 
 function App() {
-  const [inputVal, setInputVal] = useState("");
-  const dispatch = useNotification();
-
-  const handleNewNotification = () => {
-    dispatch({
-      type: "ERROR",
-      message: inputVal,
-      title: "Successful Request"
-    })
-  }
-
   return (
     <div className="App">
-      <input type="text" value={inputVal} onChange={e => setInputVal(e.target.value)}/>
-      <button onClick={handleNewNotification}>Add Notification</button>
+      <button onClick={useCreateNotification("any type", "new msg")}>Add notification</button>
     </div>
   );
 }
